496eea79ce82981c699c68f3794f9bcac465c926
yubaoquan <hzyubaoquan@corp.netease.com>
2017-08-11 01:53:25 +0800

    init

---
+++ README.md
@@ -0,0 +1,13 @@
+# backspace-until-not-whitespace package
+
+Press backspace to delete all the continuously whitespaces in the string before the cursor.
+
+![A screenshot of your package](https://raw.githubusercontent.com/yubaoquan/backspace-until-not-space/master/demo.gif)
+
+Setting:
+1. `Multi Line Delete`: check this options to enable a backspace delete until find a none-whitespace line.
+2. `Disabled File extensions`: This package will be disabled in files with suffix in this option .
+
+# Changelog
+
+0.0.1 init
---
+++ test.js
@@ -0,0 +1,94 @@
+'use babel';
+
+import { CompositeDisposable } from 'atom';
+import config from './config';
+
+export default {
+    extension: '',
+    disabledFileExtensions: [],
+    config: {
+        disabledFileExtensions: {
+            type: 'array',
+            default: ['html'],
+            description: 'Disable this package in above file types'
+        },
+        multiLineDelete: {
+            type: 'boolean',
+            default: config.multiLineDelete,
+            description: 'Disable this option to only delete in current line'
+        }
+    },
+    activate: function() {
+        this.subscriptions = new CompositeDisposable();
+        atom.config.observe('backspace-until-not-space.disabledFileExtensions', (value = []) => {
+            this.disabledFileExtensions = value;
+        });
+        atom.config.observe('backspace-until-not-space.multiLineDelete', (val) => {
+            config.multiLineDelete = val;
+        });
+        this.currentEditor = atom.workspace.getActiveTextEditor();
+        this.registerListener();
+        atom.workspace.onDidChangeActivePaneItem((paneItem) => {
+            this.currentEditor = paneItem;
+            this.registerListener();
+        });
+    },
+    registerListener() {
+        this._getFileExtension();
+        if (this.disabledFileExtensions.includes(this.extension)) {
+            console.info(`suffix:${this.extension}, do not listen`);
+            return;
+        }
+        const editor = this.currentEditor;
+        if (!editor) {
+            return;
+        }
+
+        this.buffer = editor.getBuffer();
+        this.disposer = this.buffer.onDidChangeText((e) => {
+            if (this.isDeletedSpace(e)) {
+                this.backspaceUp();
+            }
+        });
+        this.element = atom.views.getView(editor);
+        this.element.addEventListener('keydown', this.storeCursorPos.bind(this));
+    },
+    isDeletedSpace(e) {
+        if (!e || !e.changes || !e.changes[0]) {
+            return false;
+        }
+        return /^\s$/.test(e.changes[0].oldText) && e.changes[0].newText === '';
+    },
+    deactivate: function() {
+        if (this.element) {
+            this.element.removeEventListener('keydown', this.listener);
+        }
+        if (this.closeListener) {
+            this.closeListener();
+        }
+        // return this.subscriptions.dispose(); //
+        //
+    },
+    _getFileExtension: function() {
+        let filename;
+        const ref = this.currentEditor;
+        if (ref && typeof ref.getFileName === 'function') {
+            filename = ref.getFileName();
+        }
+        this.extension = undefined;
+        if (filename && filename.includes('.')) {
+            const parts = filename.split('.');
+            this.extension = parts[parts.length - 1];
+        }
+        return this.extension;
+    },
+    storeCursorPos(e) {
+        if (e.key.toLowerCase() !== 'backspace') {
+            return;
+        }
+    },
+    backspaceUp() {
+        // this.currentEditor.deleteToBeginningOfLine();
+        this.currentEditor.deleteLine();
+    },
+};
